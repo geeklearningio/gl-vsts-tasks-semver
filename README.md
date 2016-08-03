fe9672ce3449930db2e60df1d1fbd58bd2e206c3
Cyprien Autexier <cyprien.autexier@gmail.com>
2016-08-03 22:56:26 +0200

    add encode semver

---
+++ README.md
@@ -0,0 +1,16 @@
+# Semver Build and Release Tasks
+
+Visual Studio Team Services Build and Release Management extensions that help you work with semantic versioning.
+
+## Install the extension to your account
+
+You can find the latest stable version of the VSTS Extension tasks on the [Visual Studio Marketplace](https://marketplace.visualstudio.com/items?itemName=geeklearningio.gl-vsts-tasks-semver).
+
+## Prerelease channel
+
+Our tasks are build and published in continuous (thanks to VSTS, of course). If you are interested or want to contribute, you can ask us the access of the private prerelease extension to install it to your account.
+By definition, this prerelease extension could introduce breaking changes, or could be very unstable, so it is not recommended to use it for your builds and releases, but only for testing purposes.
+
+## Tasks included
+
+* **[Encode Semver To Integer](https://github.com/geeklearningio/gl-vsts-tasks-file-patch/wiki/Encode Semver To Integer)**: Encode semver to integer
---
+++ Tasks/EncodeSemverToInteger/semvertoandroidversioncode.ts
@@ -0,0 +1,71 @@
+import path = require('path');
+import fs = require('fs-extra');
+import tl = require('vsts-task-lib/task');
+import semver = require('semver');
+try {
+
+    var sourceSemVer = tl.getInput("SourceSemver");
+    var minorBits = parseInt(tl.getInput("MinorBits"));
+    var patchBits = parseInt(tl.getInput("PatchBits"));
+    var preReleaseTagBits = parseInt(tl.getInput("PreReleaseTagBits"));
+    var preReleaseTagMap = JSON.parse(tl.getInput("PreReleaseTagMap"));
+    var preReleaseNumberBits = parseInt(tl.getInput("PreReleaseNumberBits"));
+
+    var varRegex = /\$\((.*?)\)/g;
+    sourceSemVer = sourceSemVer.replace(varRegex, (match, varName, offset, string) => tl.getVariable(varName));
+
+    console.log("SourceSemVer : " + sourceSemVer);
+
+    var checkBounds = (value: number, bits: number, name: string) => {
+        if (value >= (Math.pow(2, bits))) {
+            throw new Error(name + " will overflow allocated bits. " + value.toString() + " >= " + (Math.pow(2, bits)).toString());
+        }
+    };
+
+    var majorBits = 32 - minorBits - patchBits - preReleaseNumberBits - preReleaseTagBits;
+
+    var parsedSemver = new semver.SemVer(sourceSemVer, false);
+
+    var code: number = 0;
+
+    checkBounds(parsedSemver.major, majorBits, "major");
+    code += parsedSemver.major;
+
+    checkBounds(parsedSemver.minor, minorBits, "minor");
+    code = code << minorBits;
+    code += parsedSemver.minor;
+
+    checkBounds(parsedSemver.patch, patchBits, "patch");
+    code = code << patchBits;
+    code += parsedSemver.patch;
+
+    var preReleaseTagCode: number = (2 ^ preReleaseTagBits) - 1;
+    var preReleaseNumber: number = 0;
+    if (parsedSemver.prerelease && parsedSemver.prerelease.length) {
+        var mapResult = preReleaseTagMap[parsedSemver.prerelease[0]];
+        if (mapResult) {
+            preReleaseTagCode = mapResult;
+        } else {
+            preReleaseTagCode = 0;
+        }
+        if (parsedSemver.prerelease.length > 1){
+            preReleaseNumber = parseInt(parsedSemver.prerelease[1]);
+        }
+    }
+
+    checkBounds(preReleaseTagCode, preReleaseTagBits, "preReleaseTagBits");
+    code = code << preReleaseTagBits;
+    code += preReleaseTagCode;
+
+    checkBounds(preReleaseNumber, preReleaseNumberBits, "preReleaseNumberBits");
+    code = code << preReleaseNumberBits;
+    code += preReleaseNumber;
+
+    console.log("computed versioncode : " + code.toString());
+    tl.setVariable("Android_Semver_Encoded", code.toString());
+
+    tl.setResult(tl.TaskResult.Succeeded, "Code encoded");
+} catch (err) {
+    console.error(String(err));
+    tl.setResult(tl.TaskResult.Failed, String(err));
+}
